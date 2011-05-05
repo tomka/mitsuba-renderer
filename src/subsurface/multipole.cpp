02a102a4b117cfe4025ecf321dff968b5ec76e71
Tom Kazimiers <tom@voodoo-arts.net>
2011-05-06 00:55:10 +0200

    Correct naming for dipole and multipole class files

--- src/subsurface/multipole.cpp
+++ src/subsurface/multipole.cpp
@@ -26,10 +26,10 @@ MTS_NAMESPACE_BEGIN
  * Computes the combined diffuse radiant exitance 
  * caused by a number of dipole sources
  */
-struct IsotropicMultipoleQuery {
+struct IsotropicDipoleQuery {
 #if !defined(MTS_SSE) || (SPECTRUM_SAMPLES != 3)
 	inline IsotropicDipoleQuery(const Spectrum &zr, const Spectrum &zv, 
-		const Spectrum &sigmaTr, Float Fdt, const Point &p) 
+		const Spectrum &sigmaTr, Float Fdt, const Point &p)
 		: zr(zr), zv(zv), sigmaTr(sigmaTr), result(0.0f), Fdt(Fdt), p(p) {
 			count = 0;
 	}
@@ -47,6 +47,7 @@ struct IsotropicMultipoleQuery {
 		Spectrum dMo = Spectrum(0.25f * INV_PI) *
 			 (C1 * ((-sigmaTr * dr).exp()) / (dr * dr)
 			+ C2 * ((-sigmaTr * dv).exp()) / (dv * dv));
+        
 		result += dMo * sample.E * (sample.area * Fdt);
 		count++;
 	}
@@ -78,7 +79,7 @@ struct IsotropicMultipoleQuery {
 			factor = _mm_mul_ps(_mm_set1_ps(0.25f*INV_PI*sample.area * Fdt),
 				_mm_set_ps(sample.E[0], sample.E[1], sample.E[2], 0)),
 			C1fac = _mm_div_ps(_mm_mul_ps(zr, _mm_add_ps(sigmaTr, _mm_div_ps(one, dr))), drSqr),
-			C2fac = _mm_div_ps(_mm_mul_ps(zv, _mm_add_ps(sigmaTr, _mm_div_ps(one, dv))), dvSqr);
+			C2fac = _mm_div_ps(_mm_mul_ps(zr, _mm_add_ps(sigmaTr, _mm_div_ps(one, dv))), dvSqr);
 		SSEVector temp1(_mm_mul_ps(dr, sigmaTr)), temp2(_mm_mul_ps(dv, sigmaTr));
 		const __m128
 			exp1 = _mm_set_ps(expf(-temp1.f[3]), expf(-temp1.f[2]), expf(-temp1.f[1]), 0),
@@ -101,6 +102,105 @@ struct IsotropicMultipoleQuery {
 	int count;
 	Float Fdt;
 	Point p;
+};
+
+/**
+ * Computes the combined diffuse radiant exitance 
+ * caused by a number of dipole sources
+ */
+struct IsotropicMultipoleQuery {
+//#if !defined(MTS_SSE) || (SPECTRUM_SAMPLES != 3)
+	inline IsotropicMultipoleQuery(const Spectrum &zr, const Spectrum &zv, 
+		const Spectrum &sigmaTr, Float Fdt, const Point &p, const Normal &_ns,
+        const Float _d)
+		: zr(zr), zv(zv), sigmaTr(sigmaTr), result(0.0f), Fdt(Fdt), p(p),
+          ns(_ns), d(_d) {
+		count = 0;
+	}
+
+	inline void operator()(const IrradianceSample &sample) {
+		Spectrum rSqr = Spectrum((p - sample.p).lengthSquared());
+		/* Distance to the real source */
+		Spectrum dr = (rSqr + zr*zr).sqrt();
+		/* Distance to the image point source */
+		Spectrum dv = (rSqr + zv*zv).sqrt();
+		Spectrum C1 = (sigmaTr + Spectrum(1.0f) / dr);
+		Spectrum C2 = (sigmaTr + Spectrum(1.0f) / dv);
+
+		/* Do not include the reduced albedo - will be canceled out later */
+		Spectrum dMoR = Spectrum(0.25f * INV_PI) *
+			 (zr * C1 * ((-sigmaTr * dr).exp()) / (dr * dr)
+			+ zv * C2 * ((-sigmaTr * dv).exp()) / (dv * dv));
+
+		//Spectrum dMoT = Spectrum(0.25f * INV_PI) *
+		//	 ((d - zr) * C1 * ((-sigmaTr * dr).exp()) / (dr * dr)
+		//	- (d + zv) * C2 * ((-sigmaTr * dv).exp()) / (dv * dv));
+
+        /* combine Mo based on R and Mo based on T to a new
+         * Mo based on a combined profile P. */
+        //Float cosN = dot(ns, sample.n);
+        //Spectrum dMoP = 0.5 * (cosN + 1) * dMoR
+        //    + 0.5 * (1 - cosN) * dMoT;
+
+		//result += dMoP * sample.E * (sample.area * Fdt);
+		result += dMoR * sample.E * (sample.area * Fdt);
+     
+		count++;
+	}
+
+	inline const Spectrum &getResult() const {
+		return result;
+	}
+
+	Spectrum zr, zv, sigmaTr, result;
+//#else
+//	inline IsotropicMUltipoleQuery(const Spectrum &_zr, const Spectrum &_zv, 
+//		const Spectrum &_sigmaTr, Float Fdt, const Point &p, const Normal &_ns,
+//        const Float _d) : Fdt(Fdt), p(p), ns(_ns), d(_d) {
+//		zr = _mm_set_ps(_zr[0], _zr[1], _zr[2], 0);
+//		zv = _mm_set_ps(_zv[0], _zv[1], _zv[2], 0);
+//		sigmaTr = _mm_set_ps(_sigmaTr[0], _sigmaTr[1], _sigmaTr[2], 0);
+//		zrSqr = _mm_mul_ps(zr, zr);
+//		zvSqr = _mm_mul_ps(zv, zv);
+//		result.ps = _mm_setzero_ps();
+//		count = 0;
+//	}
+//
+//	inline void operator()(const IrradianceSample &sample) {
+//		/* Distance to the positive point source of the dipole */
+//		const __m128 lengthSquared = _mm_set1_ps((p - sample.p).lengthSquared()),
+//			drSqr = _mm_add_ps(zrSqr, lengthSquared), 
+//			dvSqr = _mm_add_ps(zvSqr, lengthSquared),
+//			dr = _mm_sqrt_ps(drSqr), dv = _mm_sqrt_ps(dvSqr), 
+//			one = _mm_set1_ps(1.0f),
+//			factor = _mm_mul_ps(_mm_set1_ps(0.25f*INV_PI*sample.area * Fdt),
+//				_mm_set_ps(sample.E[0], sample.E[1], sample.E[2], 0)),
+//			C1fac = _mm_div_ps(_mm_mul_ps(distR, _mm_add_ps(sigmaTr, _mm_div_ps(one, dr))), drSqr),
+//			C2fac = _mm_div_ps(_mm_mul_ps(distV, _mm_add_ps(sigmaTr, _mm_div_ps(one, dv))), dvSqr);
+//		SSEVector temp1(_mm_mul_ps(dr, sigmaTr)), temp2(_mm_mul_ps(dv, sigmaTr));
+//		const __m128
+//			exp1 = _mm_set_ps(expf(-temp1.f[3]), expf(-temp1.f[2]), expf(-temp1.f[1]), 0),
+//			exp2 = _mm_set_ps(expf(-temp2.f[3]), expf(-temp2.f[2]), expf(-temp2.f[1]), 0);
+//		result.ps = _mm_add_ps(result.ps, _mm_mul_ps(factor, _mm_add_ps(
+//			_mm_mul_ps(C1fac, exp1), _mm_mul_ps(C2fac, exp2))));
+//	}
+//
+//	Spectrum getResult() {
+//		Spectrum value;
+//		for (int i=0; i<3; ++i)
+//			value[i] = result.f[3-i];
+//		return value;
+//	}
+//
+//	__m128 zr, zv, zrSqr, zvSqr, sigmaTr;
+//	SSEVector result;
+//#endif
+
+	int count;
+	Float Fdt;
+	Point p;
+    Normal ns;
+    Spectrum d;
 };
 
 static ref<Mutex> irrOctreeMutex = new Mutex();
@@ -133,6 +233,16 @@ public:
 		   for other interesting things.. */
 		m_ssFactor = props.getSpectrum("ssFactor", Spectrum(1.0f));
 		m_maxDepth = props.getInteger("maxDepth", 40);
+		m_extraDipoles = props.getInteger("extraDipoles", 0);
+        m_slabThickness = props.getFloat("slabThickness", 1.0f);
+        m_useMartelliD = props.getBoolean("useMartelliDC", true);
+
+        if (m_extraDipoles == 0) {
+            Log(EInfo, "Using standard dipole model");
+        } else {
+            Log(EInfo, "Using multipole model with %d dipoles in total", 2 * m_extraDipoles + 1);
+        }
+
 		/* Asymmetry parameter of the phase function */
 		m_g = props.getFloat("g", 0);
 		m_ready = false;
@@ -147,6 +257,7 @@ public:
 		m_minDelta = stream->readFloat();
 		m_maxDepth = stream->readInt();
 		m_octreeIndex = stream->readInt();
+        m_useMartelliD = stream->readBool();
 		m_ready = false;
 		m_octreeResID = -1;
 		configure();
@@ -170,29 +281,41 @@ public:
 		stream->writeFloat(m_minDelta);
 		stream->writeInt(m_maxDepth);
 		stream->writeInt(m_octreeIndex);
+        stream->writeBool(m_useMartelliD);
 	}
 
 	Spectrum Lo(const Scene *scene, const Intersection &its, const Vector &d) const {
 		if (!m_ready || m_ssFactor.isBlack())
 			return Spectrum(0.0f);
-		IsotropicDipoleQuery query(m_zr, m_zv, m_sigmaTr, m_Fdt, its.p);
-	
-		const Normal &n = its.shFrame.n;
-		m_octree->execute(query);
 
-        // compute multiple scattering term
-        Spectrum Sd = query.getResult();
-        // compute single scattering term
-        Spectrum S1 = Spectrum(0);
+        const Normal &n = its.shFrame.n;
 
-        // compute total BSSRDF
-        Spectrum S = Sd + S1;
+        Spectrum Mo = Spectrum(0.0f);
+        /* If there are no extra dipoles, do a srandard dipolo query */
+        if (m_extraDipoles == 0) {
+                IsotropicDipoleQuery query(m_zr[0], m_zv[0], m_sigmaTr, m_Fdt, its.p);
+                m_octree->execute(query);
+                // compute combined radiant exitance
+                Mo += query.getResult();
+        } else {
+            for (int i=-m_extraDipoles; i<=m_extraDipoles; ++i) {
+                int idx = i + m_extraDipoles;
+                // calulate diffuse reflectance and transmittance
+                IsotropicMultipoleQuery query(m_zr[idx], m_zv[idx], m_sigmaTr, m_Fdt, its.p, n, m_slabThickness);
+                m_octree->execute(query);
+
+                // compute combined radiant exitance
+                Mo += query.getResult();
+//std::cerr << i << ": " <<  query.count << std::endl;
+            }
+        }
+
 
 		if (m_eta == 1.0f) {
-			return S * m_ssFactor * INV_PI;
+			return Mo * m_ssFactor * INV_PI;
 		} else {
 			Float Ft = 1.0f - fresnel(absDot(n, d));
-			return S * m_ssFactor * INV_PI * (Ft / m_Fdr);
+			return Mo * m_ssFactor * INV_PI * (Ft / m_Fdr);
 		}
 	}
 
@@ -237,12 +360,30 @@ public:
 		/* Effective transport extinction coefficient */
 		m_sigmaTr = (m_sigmaA * m_sigmaTPrime * 3.0f).sqrt();
 
-		/* Diffusion coefficient */
-		m_D = Spectrum(1.0f) / (m_sigmaTPrime * 3.0f);
+		/* Diffusion coefficient
+         * According to Martelli et al.'s paper "Accuracy of the Diffusion
+         * Equation to Describe Photon Migration through an Infininite
+         * Medium" from 2000, the diffusion coefficient should be calculated
+         * slightly different. In practice this seems only required when
+         * sigmaA / sigmaSPrime > 0.01.
+         */
+        if (m_useMartelliD)
+		    m_D = Spectrum(1.0f) / (m_sigmaSPrime * 3.0f + m_sigmaA);
+        else
+		    m_D = Spectrum(1.0f) / (m_sigmaTPrime * 3.0f);
+
+        /* Fluence vanishing point */
+        Spectrum zb = 2 * m_A * m_D;
+        /* Needed for calculation purposes */
+        Spectrum d = Spectrum(m_slabThickness);
 
 		/* Distance of the dipole point sources to the surface */
-		m_zr = m_mfp; 
-		m_zv = m_mfp * (1.0f + 4.0f/3.0f * m_A);
+        for (int i=-m_extraDipoles; i<=m_extraDipoles; ++i) {
+            Float dipoleIdx = 2 * i;
+            Spectrum basicDist = dipoleIdx * d + dipoleIdx * 2 * zb;
+            m_zr.push_back( basicDist + m_mfp );
+            m_zv.push_back( basicDist - m_mfp - 2 * zb );
+        }
 	}
 
 	/// Unpolarized fresnel reflection term for dielectric materials
@@ -330,13 +471,25 @@ public:
 private:
 	Float m_minMFP, m_sampleMultiplier;
 	Float m_Fdr, m_Fdt, m_A, m_minDelta, m_g;
-	Spectrum m_mfp, m_sigmaTr, m_zr, m_zv, m_alphaPrime;
+	Spectrum m_mfp, m_sigmaTr, m_alphaPrime;
 	Spectrum m_sigmaSPrime, m_sigmaTPrime, m_D, m_ssFactor;
+    /* True if alternate diffusion constant calculation should be used */
+    bool m_useMartelliD;
+    /* Lists for the dipole distances */
+    std::vector<Spectrum> m_zr, m_zv;
 	ref<IrradianceOctree> m_octree;
 	ref<ParallelProcess> m_proc;
 	int m_octreeResID, m_octreeIndex;
 	int m_maxDepth;
 	bool m_ready, m_requireSample;
+    /* The number of additional dipoles. Specifies the number of
+     * dipoles that get added below *and* above the surface. E.g.
+     * 1 additional dipole means you have the standard dipole plus
+     * one above and one below.
+     */
+    int m_extraDipoles;
+    /* The slab thickness */
+    Float m_slabThickness;
 };
 
 MTS_IMPLEMENT_CLASS_S(IsotropicDipole, false, Subsurface)
